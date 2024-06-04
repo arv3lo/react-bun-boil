21d464c636ae00a1a358a4b521504ef0e8091676
gérald <arivelo.live@gmail.com>
2024-06-04 14:10:40 +0300

    added basic tailwind configs & docker files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { configDefaults } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    ...configDefaults,
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      enabled: true,
+      reporter: ["html", "json", "json-summary"],
+      reportOnFailure: true,
+      thresholds: {
+        statements: 25,
+        functions: 25,
+        branches: 25,
+        lines: 25,
+      },
+    },
+  },
+});
